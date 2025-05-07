168660ea2f0193202d53e762a42d394a02ef6329
reii0x0 <nghimegamede55@gmail.com>
2025-05-07 11:00:54 +0700

    xong het r ne` con deploy nua~ hoy

--- components/ui/media-room.tsx
+++ components/ui/media-room.tsx
@@ -22,26 +22,36 @@ export const MediaRoom = ({
     const [token, setToken] = useState("");
     console.log("isLoaded:", isLoaded);
     console.log("user:", user);
-
+    console.log("user.firstName:", user?.firstName);
+    console.log("user.lastName:", user?.lastName);
     useEffect(() => {
-        if (!isLoaded || !user?.firstName || !user?.lastName) {
+        console.log("useeffect triggered");
+        
+        if (!isLoaded || !user) {
             console.log("User not loaded");
             return;
         }
         
-        const name = `${user.firstName} ${user.lastName}`;
+        const name = `${user.firstName ?? "Unknown"} ${user.lastName ?? "User"} `;
         console.log("Calling api with name: ", name);
 
         (async () => {
             try {
                 const resp = await fetch(`/api/livekit?room=${chatId}&username=${name}`)
                 const data = await resp.json();
-                setToken(data.token);
+                if (!data?.token) {
+                    console.log("No token found in response:", data);
+                    return;
+                }
+                console.log("Token response object:", data);
+                console.log("typeof token:", typeof data.token);
+                setToken(data.token);   
+                console.log("Token received:", data.token);
             } catch (err) {
                 console.log(err);
             }
         })()
-    }, [isLoaded, user?.firstName, user?.lastName, chatId]);
+    }, [isLoaded, user, chatId]);
 
     if (!isLoaded || token === "") {
         return (
@@ -53,6 +63,7 @@ export const MediaRoom = ({
             </div>
         )
     }
+    console.log("serverUrl:", process.env.NEXT_PUBLIC_LIVEKIT_URL);
 
 
     return (
